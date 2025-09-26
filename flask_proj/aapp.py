4c3a26bfd548d345e4fe7e1533aa6c29ae84831b
unknown <chizubeonuorah@gmail.com>
2025-09-26 12:46:23 +0100

    cors

--- flask_proj/aapp.py
+++ flask_proj/aapp.py
@@ -1,8 +1,11 @@
 import datetime
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import requests
 
 app = Flask(__name__)
+
+CORS(app, resources=r'*', supports_credentials=True)
 
 # --- Mock Data (simulating a database or live weather service) ---
 
